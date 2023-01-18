1d75730f9f8776743ba216570df2e3ecc3843e80
tampopo <67646094+tampopo256@users.noreply.github.com>
2023-01-19 08:54:45 +0900

    First Commit

---
+++ js/tp256.js
@@ -0,0 +1,5 @@
+const style = `color:red; background:white; font-size:32px;`;
+
+console.log("%cあなたがこの画面を見ていることは悪いことではありません\nしかし、この画面で得られる情報は開発者しか興味の無いはずです\nもしあなたが開発者ならば、一緒にコードを書きましょう！\np202114032@sekishoko-h.ed.jp", style);
+
+jQuery( 'img[usemap]' ).rwdImageMaps();
