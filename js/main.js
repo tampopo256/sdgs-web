a6779e1a2a08159b47508831dea8e35568f0f36f
tampopo256 <p202114032@sekishoko-h.ed.jp>
2023-01-20 22:53:46 +0900

    Backup Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// SDGsの画像サイズ変更を禁止
+jQuery( 'img[usemap]' ).rwdImageMaps();
+
+// console.logの描画指定
+const style = `color:red; background:white; font-size:17px;`;
+
+// しゅつりょく！！
+console.log("%cあなたがこの画面を見ていることは悪いことではありません\nしかし、この画面で得られる情報は開発者しか興味の無いはずです\nもしあなたが開発者ならば、一緒にコードを書きましょう！", style);
